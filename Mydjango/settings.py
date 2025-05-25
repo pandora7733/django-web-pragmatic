e3cb329b5ef4fd8033596a9d41d4eb62f380c144
coding_sihoo <pelomon73@gmail.com>
2025-05-25 22:25:27 +0900

    change STATIC_ROOT: staticfiles -> staticfiles_docker

--- Mydjango/settings.py
+++ Mydjango/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_docker')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
